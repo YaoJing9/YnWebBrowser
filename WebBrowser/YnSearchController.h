9a531124cb8b45e28c234e2929c2a5de3ba3a23e
YaoJing9 <1178031378@qq.com>
2017-11-23 11:07:59 +0800

    no message

---
+++ WebBrowser/YnSearchController.h
@@ -0,0 +1,13 @@
+//
+//  YnSearchController.h
+//  WebBrowser
+//
+//  Created by yaojing on 2017/11/23.
+//  Copyright © 2017年 钟武. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface YnSearchController : BaseViewController
+
+@end
---
+++ WebBrowser/YnSearchController.m
@@ -0,0 +1,25 @@
+//
+//  YnSearchController.m
+//  WebBrowser
+//
+//  Created by yaojing on 2017/11/23.
+//  Copyright © 2017年 钟武. All rights reserved.
+//
+
+#import "YnSearchController.h"
+
+@interface YnSearchController ()
+
+@end
+
+@implementation YnSearchController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
