331b9d47647a95d6e0350ad5f02a793830bd86c2
YaoJing9 <1178031378@qq.com>
2017-11-23 11:08:42 +0800

    no message

--- WebBrowser/YnSearchController.m
+++ WebBrowser/YnSearchController.m
@@ -9,7 +9,18 @@
 #import "YnSearchController.h"
 
 @interface YnSearchController ()
-
+@property (nonatomic, strong) NSMutableArray *dataSource;
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) UIView *tagsView;
+@property (nonatomic, strong) UIView *headerView;
+/** 搜索历史 */
+@property (nonatomic, strong) NSMutableArray *searchHistories;
+/** 搜索历史缓存保存路径, 默认为PYSEARCH_SEARCH_HISTORY_CACHE_PATH(PYSearchConst.h文件中的宏定义) */
+@property (nonatomic, copy) NSString *searchHistoriesCachePath;
+/** 搜索历史记录缓存数量，默认为20 */
+@property (nonatomic, assign) NSUInteger searchHistoriesCount;
+/** 搜索建议（推荐）控制器 */
+@property (nonatomic, weak) MLSearchResultsTableViewController *searchSuggestionVC;
 @end
 
 @implementation YnSearchController
