9b27a38bbd9968c49ad61acb6c84ef83bcc138b4
jiangdongxu <jiangdongxu1230@sina.com>
2017-11-30 15:24:13 +0800

    修复BUG

--- WebBrowser/FirstBrowserController.m
+++ WebBrowser/FirstBrowserController.m
@@ -108,7 +108,6 @@
     
     WebModel *webModel = [[TabManager sharedInstance] getCurrentWebModel];
     webModel.image = [[SaveImageTool sharedInstance] GetImageFromLocal:@"firstImage"];
-    
     webModel.isNewWebView = YES;
     
     [[TabManager sharedInstance] setMultiWebViewOperationBlockWith:^(NSArray<WebModel *> *array) {
